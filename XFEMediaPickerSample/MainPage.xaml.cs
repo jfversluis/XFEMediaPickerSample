81b8b72fef9cf4a8a459aa69910b596743ae8e65
Gerald Versluis <gerald@verslu.is>
2020-09-21 15:54:17 +0200

    Implement MediaPicker

--- XFEMediaPickerSample/MainPage.xaml.cs
+++ XFEMediaPickerSample/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace XFEMediaPickerSample
@@ -13,6 +14,33 @@ namespace XFEMediaPickerSample
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            {
+                Title = "Please pick a photo"
+            });
+
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+
+                resultImage.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+
+        async void Button1_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var result = await MediaPicker.CapturePhotoAsync();
+
+            if (result != null)
+            {
+                var stream = await result.OpenReadAsync();
+
+                resultImage.Source = ImageSource.FromStream(() => stream);
+            }
         }
     }
 }
